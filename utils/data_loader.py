8327072e07279582e37edde0ef3e3e415fb9f241
Justin Ho <juntingho4@gmail.com>
2026-06-22 19:32:42 +0800

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -65,9 +65,7 @@ def parse_gpx(gpx_filename):
     if not df.empty:
         df['timestamp'] = pd.to_datetime(df['timestamp'])
     if df['timestamp'].dt.tz is not None:
-        df['timestamp'] = (df['timestamp'].dt.tz_convert('Asia/Singapore').dt.tz_localize(None))
-    else:
-        df['timestamp'] = df['timestamp'] + pd.Timedelta(hours=8)
+        df['timestamp'] = df['timestamp'].dt.tz_localize(None)
     return df
 
 def parse_fit(fit_filename):
