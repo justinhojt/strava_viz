0f71cad80a2d97f616d1095d94abf9a5d04f9c92
Justin Ho <juntingho4@gmail.com>
2026-07-03 17:40:44 +0800

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -11,18 +11,15 @@ csv = config.ACTIVITIES_CSV
 
 def process_timestamps(df):
     if not df.empty:
-        df['timestamp'] = pd.to_datetime(df['timestamp'])
-        if df['timestamp'].dt.tz is not None:
-            df['timestamp'] = df['timestamp'].dt.tz_convert('Asia/Singapore').dt.tz_localize(None)
-        else:
-            df['timestamp'] = df['timestamp'] + pd.Timedelta(hours=8)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
+        df['timestamp'] = df['timestamp'].dt.tz_convert('Asia/Singapore').dt.tz_localize(None)
         df['graph_timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%dT%H:%M:%S')
     return df
 
 @st.cache_data
 def parse_csv():
     if not os.path.exists(csv):
-        raise FileNotFoundError(f'Could not find activities.csv at {csv}')
+        raise FileNotFoundError(f'Could not find activities dataset at {csv}')
         
     df = pd.read_csv(csv)
     df['Activity Date'] = pd.to_datetime(df['Activity Date'])
