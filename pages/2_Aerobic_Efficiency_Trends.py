7e1bd9aedfc7585f898cf42e31f0b6d872e97230
Justin Ho <juntingho4@gmail.com>
2026-07-03 17:31:51 +0800

    Update 2_Aerobic_Efficiency_Trends.py

--- pages/2_Aerobic_Efficiency_Trends.py
+++ pages/2_Aerobic_Efficiency_Trends.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from scikit-learn.linear_model import LinearRegression
 
 from utils.data_loader import parse_csv
 from utils.plots import plot_aero
